69c4a0dd5d15c338a6b76be5645c841e5d66399f
Chi Hoang <chihoang@Chis-iMac.hsd1.ca.comcast.net>
2017-10-09 21:52:52 -0700

    Add Register page and bind data
    Add a service to validate form and email with flash messages

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  password: String;
+  email: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, private flashMessages:FlashMessagesService) { }
 
   ngOnInit() {
   }
+  
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      username: this.username,
+      password: this.password,
+      email: this.email
+    };
 
+    // Require fields
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessages.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    // Validate email
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessages.show('Email is not valid', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+  }
 }
